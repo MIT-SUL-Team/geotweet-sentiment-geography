927d9715121d3d31c962823e6ad5391ff2017653
Nicolas Guetta Jeanrenaud <nicolas.jeanrenaud@gmail.com>
2021-01-12 10:57:18 +0100

    troubleshooting

--- src/utils/emb_sentiment_imputer.py
+++ src/utils/emb_sentiment_imputer.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import numpy as np
-import sklearn
 import argparse
-from sentence_transformers import SentenceTransformer
 import joblib
 from tqdm.auto import tqdm
 import torch
