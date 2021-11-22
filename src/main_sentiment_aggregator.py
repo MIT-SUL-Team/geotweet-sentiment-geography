142f403330e2a1c1dd3d4958b4ddedb4b80ae682
Nicolas Guetta Jeanrenaud <nicolas.jeanrenaud@gmail.com>
2021-11-22 09:29:15 -0500

    update

--- src/main_sentiment_aggregator.py
+++ src/main_sentiment_aggregator.py
@@ -1,4 +1,4 @@
-#usage: python src/main_sentiment_aggregator.py twitter --countries USA --tweet_text_path /home/sentiment/data-lake/twitter/processed/ --tweet_geo_path /home/sentiment/data-lake/twitter/geoinfo/
+#usage: python src/main_sentiment_aggregator.py --countries USA --text_path /home/sentiment/data-lake/twitter/processed/ --geo_path /home/sentiment/data-lake/twitter/geoinfo/
 
 import pandas as pd
 import numpy as np
@@ -9,21 +9,21 @@ from utils.aggregation_utils import run_aggregation
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('platform', help='Which social media data are we using (twitter, weibo)?')
-    parser.add_argument('--countries', nargs='*', default='', help='Country abbreviations (3 letter)')
+    parser.add_argument('--platform', default='twitter', help='Which social media data are we using (twitter, weibo)?')
     parser.add_argument('--text_path', default='', type=str, help='path to tweet text data')
     parser.add_argument('--geo_path', default='', type=str, help='path to tweet geography data')
     parser.add_argument('--sent_path', default='', type=str, help='path to tweet sentiment data')
     parser.add_argument('--sentiment_method', default='bert', help='Which sentiment imputation method?')
+    parser.add_argument('--start_date', default='2019-01-01', type=str, help='Start date')
+    parser.add_argument('--end_date', default='2020-09-30', type=str, help='End date')
+    parser.add_argument('--countries', nargs='*', default='', help='Country abbreviations (3 letter)')
     parser.add_argument('--geo_level', default='admin1', type=str, help='level of geo granularity')
     parser.add_argument('--time_level', default='day', type=str, help='level of time granularity')
+    parser.add_argument('--incl_keywords', nargs='*', default='', help='Which keywords do you want to include in the subset?')
+    parser.add_argument('--excl_keywords', nargs='*', default='', help='Which keywords do you want to exclude in the subset?')
     parser.add_argument('--ind_robust_threshold', default=3, type=int, help='How many tweets for an individual to be considered robust?')
     parser.add_argument('--ind_level', default=False, type=bool, help='Would you like the data aggregated at individual level?  (produces larger files)')
     parser.add_argument('--lang_level', default=False, type=bool, help='Would you like the data aggregated by languages? (produces larger files)')
-    parser.add_argument('--start_date', default='2019-01-01', type=str, help='Start date')
-    parser.add_argument('--end_date', default='2020-09-30', type=str, help='End date')
-    parser.add_argument('--incl_keywords', nargs='*', default='', help='Which keywords do you want to include in the subset?')
-    parser.add_argument('--excl_keywords', nargs='*', default='', help='Which keywords do you want to exclude in the subset?')
     parser.add_argument('--subset_usernames_file', default='', help='Would you like to subset to a list of usernames?')
     parser.add_argument('--name_ext', default='', type=str, help='File name extension')
     args = parser.parse_args()
