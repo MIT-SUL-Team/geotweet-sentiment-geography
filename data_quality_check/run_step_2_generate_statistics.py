b6722b05e489ee5437cee88b67e75a398e58e587
Sirena Yu <yusirena4869@gmail.com>
2022-02-28 12:18:24 -0500

    one slight change in run step 2

--- data_quality_check/run_step_2_generate_statistics.py
+++ data_quality_check/run_step_2_generate_statistics.py
@@ -4,7 +4,7 @@
 # @Date       : 2022-02-25 16:32
 # @Description:
 
-from data_quality_check.func_statistics import iterate_files
+from func_statistics import iterate_files
 
 # Step 1: Number of post statistics / sentiment trend
 PARAMETER_YEAR = None
